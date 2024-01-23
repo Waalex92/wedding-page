dc93874b987568ddcdea9975591ab7c219dc5b49
Alex Wong <alex.wong.aguilar@gmail.com>
2024-01-23 00:13:12 -0400

    modified:   src/app/_action.ts

--- src/app/_action.ts
+++ src/app/_action.ts
@@ -20,7 +20,7 @@ export async function addEntry(data: Inputs) {
 }
 
 type ContactFormInputs = z.infer<typeof ContactFormSchema>
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.re_5MtXruHK_MttBT5qUbp4JkVD2k9S4224h)
 
 export async function sendEmail(data: ContactFormInputs) {
     const result = ContactFormSchema.safeParse(data)
