613bf4961c5fc55208055f8b5496c6d877ec8deb
Alex Wong <alex.wong.aguilar@gmail.com>
2024-01-24 17:15:31 -0400

    modified:   src/app/api/route.ts 	deleted:    src/components/Forms/with-actions.tsx 	modified:   src/lib/schema.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,27 +3,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 import { Resend } from 'resend';
-import { ContactFormSchema, FormDataSchema } from '@/lib/schema';
+import { ContactFormSchema } from '@/lib/schema';
 import ContactFormEmail from '@/emails/contact-form-email';
-
-type Inputs = z.infer<typeof FormDataSchema>;
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const data: Inputs = req.body;
-
-        const result = FormDataSchema.safeParse(data);
-
-        if (result.success) {
-            // Aquí puedes hacer lo que necesites con los datos
-            res.status(200).json({ success: true, data: result.data });
-        } else {
-            res.status(400).json({ success: false, error: result.error?.format() });
-        }
-    } else {
-        res.status(405).json({ success: false, error: 'Method Not Allowed' });
-    }
-}
 
 type ContactFormInputs = z.infer<typeof ContactFormSchema>;
 const resend = new Resend(process.env.RESEND_API_KEY);
