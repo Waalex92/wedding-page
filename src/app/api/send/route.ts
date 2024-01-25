fb707d7655cbd5a57d359b36b1203a5a22d6ec8d
Alex Wong <alex.wong.aguilar@gmail.com>
2024-01-24 21:00:18 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/api/send/route.ts 	deleted:    src/app/contact/page.tsx 	modified:   src/components/Forms/contact-form.tsx

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,6 +1,4 @@
-'use server'
 
-import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 import { Resend } from 'resend';
 import { ContactFormSchema } from '@/lib/schema';
@@ -9,24 +7,18 @@ import ContactFormEmail from '@/emails/contact-form-email';
 type ContactFormInputs = z.infer<typeof ContactFormSchema>;
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail(data: ContactFormInputs) {
-    const result = ContactFormSchema.safeParse(data);
-
-    if (result.success) {
-        const { name, email, message } = result.data;
-        try {
-            const emailData = await resend.emails.send({
-                from: 'Wishes <onboarding@resend.dev>',
-                to: ['alex.wong.aguilar@gmail.com'],
-                subject: 'Deseos para los novios',
-                text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-                react: ContactFormEmail({ name, email, message }),
-            });
-            return { success: true, data: emailData };
-        } catch (error) {
-            return { success: false, error };
-        }
-    } else {
-        return { success: false, error: result.error?.format() };
+export async function POST(request) {
+    try {
+        const body = await request.json();
+        const { name, email, message } = body;
+        const data = await resend.emails.send({
+            from: 'Wishes <onboarding@resend.dev>',
+            to: ['alex.wong.aguilar@gmail.com'],
+            subject: 'Deseos para los novios',
+            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+            react: ContactFormEmail({ name, email, message }),
+        });
+    } catch (error) {
+        return { success: false, error };
     }
 }
--- src/components/Forms/contact-form.tsx
+++ src/components/Forms/contact-form.tsx
@@ -5,80 +5,116 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { ContactFormSchema } from "@/lib/schema";
 import { toast } from "sonner";
-import { sendEmail } from "@/app/api/send/route";
+import { useState, ChangeEvent, FormEvent } from "react";
+
+interface FormData {
+	name: string;
+	email: string;
+	message: string;
+}
 
 export type ContactFormInputs = z.infer<typeof ContactFormSchema>;
 
 export default function ContactForm() {
+	const [formData, setFormData] = useState<FormData>({
+		name: "",
+		email: "",
+		message: "",
+	});
+
 	const {
 		register,
-		handleSubmit,
 		reset,
 		formState: { errors, isSubmitting },
 	} = useForm<ContactFormInputs>({
 		resolver: zodResolver(ContactFormSchema),
 	});
 
-	const processForm: SubmitHandler<ContactFormInputs> = async (data) => {
-		const result = await sendEmail(data);
+	// Manejar cambios en los inputs
+	const handleInputChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		const { name, value } = e.target;
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[name]: value,
+		}));
+	};
 
-		if (result?.success) {
-			console.log({ data: result.data });
-			toast.success("Email sent!");
-			reset();
-			return;
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		// Puedes realizar operaciones asíncronas aquí
+		// Por ejemplo, enviar los datos a un servidor
+		try {
+			// Simular una operación asíncrona, por ejemplo, una solicitud HTTP
+			await enviarDatosAlServidor(formData);
+
+			console.log("Formulario enviado con éxito");
+		} catch (error) {
+			console.error("Error al enviar el formulario", error);
 		}
+	};
 
-		// toast error
-		console.log(result?.error);
-		toast.error("Something went wrong!");
+	const enviarDatosAlServidor = async (datos: FormData) => {
+		try {
+			const response = await fetch("/api/send", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(datos),
+			});
+
+			if (!response.ok) {
+				throw new Error(
+					`Error en la solicitud: ${response.statusText}`
+				);
+			}
+
+			const result = await response.json();
+			return result;
+		} catch (error) {
+			console.error("Error al enviar la solicitud al servidor", error);
+			throw error;
+		}
 	};
 
 	return (
 		<form
-			onSubmit={handleSubmit(processForm)}
+			onSubmit={handleSubmit}
 			className="m-4 flex flex-1 flex-col gap-4 ">
 			<div>
 				<input
 					placeholder="Nombre"
 					className="w-full rounded-lg border-2 border-blue-300 p-2 text-blue-400 text-xl"
-					{...register("name")}
+					type="text"
+					name="name"
+					value={formData.name}
+					onChange={handleInputChange}
 				/>
-				{errors.name?.message && (
-					<p className="ml-1 mt-1 text-xl text-red-400">
-						{errors.name.message}
-					</p>
-				)}
 			</div>
-
 			<div>
 				<input
 					placeholder="Correo"
 					className="w-full rounded-lg border-2 border-blue-300 p-2 text-blue-400 text-xl"
-					{...register("email")}
+					type="email"
+					name="email"
+					value={formData.email}
+					onChange={handleInputChange}
 				/>
-				{errors.email?.message && (
-					<p className="ml-1 mt-1 text-xl text-red-400">
-						{errors.email.message}
-					</p>
-				)}
 			</div>
-
 			<div>
 				<textarea
 					rows={5}
 					cols={5}
 					placeholder="Mensaje"
 					className="w-full rounded-lg p-2 border-2 border-blue-300 text-blue-400 text-xl"
-					{...register("message")}
+					name="message"
+					value={formData.message}
+					onChange={handleInputChange}
 				/>
-				{errors.message?.message && (
-					<p className="ml-1 text-xl text-red-400">
-						{errors.message.message}
-					</p>
-				)}
 			</div>
-
 			<button
 				disabled={isSubmitting}
 				className="rounded-lg border border-blue-400 bg-sky-100 py-2.5 font-medium text-blue-400 text-4xl transition-colors hover:bg-black/80 disabled:cursor-not-allowed disabled:opacity-50 mb-10">
